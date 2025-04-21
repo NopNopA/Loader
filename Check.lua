1f0de26951f80218351cb7581ec6fb37009dd728
NopNopA <134138698+NopNopA@users.noreply.github.com>
2025-04-21 15:02:17 +0700

    Update Check.lua

--- Check.lua
+++ Check.lua
@@ -1,17 +1,20 @@
 local Request = (syn and syn.request) or request or (http and http.request) or http_request
 local username = game.Players.LocalPlayer.Name
 local url = "http://103.216.158.53:12345/online"
+local HttpService = game:GetService("HttpService")
 
 while true do
-    Request({
-        Url = url,
-        Method = "POST",
-        Headers = {
-            ["Content-Type"] = "application/json"
-        },
-        Body = game:GetService("HttpService"):JSONEncode({
-            username = username
+    pcall(function()
+        Request({
+            Url = url,
+            Method = "POST",
+            Headers = {
+                ["Content-Type"] = "application/json"
+            },
+            Body = HttpService:JSONEncode({
+                username = username
+            })
         })
-    })
+    end)
     wait(50)
 end
